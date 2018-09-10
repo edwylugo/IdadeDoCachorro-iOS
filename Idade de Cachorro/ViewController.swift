95a07a0fc0cc5488e85eab8ba5e97bfbefaf5a73
Edwy Lugo <edwy@sdvirtua.com.br>
2018-09-09 23:24:39 -0300

    Initial Commit

---
+++ Idade de Cachorro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Idade de Cachorro
+//
+//  Created by Edwy Lugo on 09/09/2018.
+//  Copyright © 2018 SDvirtua Marketing Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Idade de CachorroTests/Idade_de_CachorroTests.swift
@@ -0,0 +1,36 @@
+//
+//  Idade_de_CachorroTests.swift
+//  Idade de CachorroTests
+//
+//  Created by Edwy Lugo on 09/09/2018.
+//  Copyright © 2018 SDvirtua Marketing Digital. All rights reserved.
+//
+
+import XCTest
+@testable import Idade_de_Cachorro
+
+class Idade_de_CachorroTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
